5c812645174dc873d6882050b41dcb16fe14b2bd
shangxianw <997049907@qq.com>
2022-10-01 01:35:40 +0800

    添加主路由

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cluster = require("cluster");
 
 if (cluster.isMaster) {
-	const numCPUs = require('os').cpus().length;
+	const numCPUs = require("os").cpus().length;
 	console.log(`主进程 ${process.pid} 正在运行`);
 
 	for (let i = 0; i < numCPUs; i++) {
@@ -23,12 +23,15 @@ else {
 	app.listen(80, () => {
 		console.log(`服务器已启动, 访问地址为 http://localhost:80`)
 	});
-	app.all("*", async (req, res, next) => {
+	app.all("*", (req, res, next) => {
 		res.header("Access-Control-Allow-Origin", "*"); // http://localhost:80
 		res.header("Access-Control-Allow-Methods", "GET, POST");
 		res.header("Access-Control-Allow-Credentials", "true");
 		res.header("Access-Control-Allow-Headers", "Content-Type");
+		next();
+	})
 
-		res.send("hello app");
+	app.get("/", (req, res) => {
+		res.send("hello towser");
 	})
 }
